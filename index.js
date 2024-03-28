af559f299a3447dc4cea8b6d196bbc4d9b3521c2
Ru <lulu802790@gmail.com>
2024-03-28 15:18:42 +0800

    第一次commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import data1 from "./singers.json" assert { type: "json" };
+
+const { singers } = data1;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("網站首頁");
+});
+
+// /singer/:id.html
+app.get("/singer/:id.html", (req, res) => {
+  const { id } = req.params;
+  let result = singers.find((singer) => singer.id === parseInt(id));
+  if (!result) {
+    res.status(404).send("<h1>找不到歌手</h1>");
+    return false;
+  }
+  res.send(`<!DOCTYPE html>
+  <html lang="zh-hant-TW">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${result.singer_name} 的頁面</title>
+  </head>
+  <body>
+    <h1>${result.singer_name} 的頁面</h1>
+    <img src="${result.singer_img}" alt="">
+  </body>
+  </html>`);
+});
+
+app.listen(3000, () => {
+  console.log("server is running at http://localhost:3000");
+});
